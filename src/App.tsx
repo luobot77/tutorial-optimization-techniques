751a8628d8be63d4eeb8ec302b08735ae2be3658
Jason <jsyzmq9328@gmail.com>
2023-07-10 23:17:51 +0800

    feat: add toggle paragraph functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import Button from './components/UI/Button';
 import styles from './App.module.css';
 
 const App = () => {
+  console.log('App Component Rendered');
+
+  const [isParagraphShown, setIsParagraphShown] = useState(false);
+
+  const handleToggleParagraph = () => {
+    setIsParagraphShown((prevState) => !prevState);
+  };
+
   return (
     <div className={styles.app}>
       <h1>Hi there!</h1>
+      <Button onClick={handleToggleParagraph} disabled={false}>
+        Toggle Paragraph
+      </Button>
+      {isParagraphShown && <p>This is new!</p>}
     </div>
   );
 };
